63cc9144b038d8562af23253f24f4ec05feb799c
samuelajayii <ajayisamuel023@gmail.com>
2024-09-11 15:10:05 +0100

    my first solidity commit

---
+++ src/FundMe.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+// 438,734
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+    uint256 public constant MINIMUM_USD = 5e18;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded)
+        public addressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() > 1e18, "Didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        return priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "You are not the owner");
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
---
+++ src/PriceConverter.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter{
+    function getPrice() internal view returns (uint256) {
+        // Address 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF 
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        (,int256 price ,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+
+    }
+
+    function getConversionRate(uint256 _ethAmount) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * _ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+
+    function getVersion() internal view returns (uint256){
+        return AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF).version();
+    }
+
+}
